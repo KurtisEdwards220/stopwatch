8e1b83030b0ed59f2f757c73fb8c5eaaccb2524f
KurtisEdwards220 <edwards.kurtis@gmail.com>
2024-04-27 18:34:08 -0600

    Update App.js to display a stopwatch instead of the default content

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [time, setTime] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>stopwatch</h1>
+      <div>
+        <span>{'0' + Math.floor((time / 60000) % 60)}:</span>
+        <span>{'0' + Math.floor((time / 1000) % 60)}:</span>
+        <span>{'0' + ((time / 10) % 1000)}:</span>
+      </div>
+    </>
   );
 }
 
