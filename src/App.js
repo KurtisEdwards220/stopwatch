afe423d08270ada581be69f2e61ce403e8dfc264
KurtisEdwards220 <edwards.kurtis@gmail.com>
2024-04-27 19:34:49 -0600

    Refactor stopwatch logic in App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   const [time, setTime] = useState(0);
   const [running, setRunning] = useState(false);
+
+  useEffect(() => {
+    let interval;
+    if (running) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 10);
+      }, 10);
+    } else if (!running) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [running]);
+
   return (
     <>
       <h1>stopwatch</h1>
       {/* Time logic */}
       <div>
-        <span>{'0' + Math.floor((time / 60000) % 60)}:</span>
-        <span>{'0' + Math.floor((time / 1000) % 60)}:</span>
-        <span>{'0' + ((time / 10) % 1000)}:</span>
+        <span>{('0' + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+        <span>{('0' + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
+        <span>{('0' + ((time / 10) % 100)).slice(-2)}:</span>
       </div>
       {/* Button logic */}
       <div>
