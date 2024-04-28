3077f63a18fed9889fa0bad4acfba25725a2fb8a
KurtisEdwards220 <edwards.kurtis@gmail.com>
2024-04-27 19:07:11 -0600

    Add stopwatch button functionality to App.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,42 @@ import { useState } from 'react';
 
 function App() {
   const [time, setTime] = useState(0);
+  const [running, setRunning] = useState(false);
   return (
     <>
       <h1>stopwatch</h1>
+      {/* Time logic */}
       <div>
         <span>{'0' + Math.floor((time / 60000) % 60)}:</span>
         <span>{'0' + Math.floor((time / 1000) % 60)}:</span>
         <span>{'0' + ((time / 10) % 1000)}:</span>
+      </div>
+      {/* Button logic */}
+      <div>
+        {/* Start Button */}
+        <button
+          onClick={() => {
+            setRunning(true);
+          }}
+        >
+          Start
+        </button>
+        {/* Stop Button */}
+        <button
+          onClick={() => {
+            setRunning(false);
+          }}
+        >
+          Stop
+        </button>
+        {/* Reset Button */}
+        <button
+          onClick={() => {
+            setTime(0);
+          }}
+        >
+          Reset
+        </button>
       </div>
     </>
   );
